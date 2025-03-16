7038232e5b88bf166a1470f79d025ba6a003413d
ahmar-husain <ahmar.husain@maersk.com>
2025-03-16 13:49:37 +0530

    final

--- src/api/Controllers/ExpertController.cs
+++ src/api/Controllers/ExpertController.cs
@@ -7,7 +7,7 @@ namespace api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ExpertController(IExpertService expertService) : ControllerBase
+    public class ExpertController(IExpertService expertService,IVerseService verseService) : ControllerBase
     {
         [HttpPost("chat")]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -16,6 +16,15 @@ namespace api.Controllers
            ChatMessageContent resposne= await expertService.GetReponse(expertChatRequest.message, expertChatRequest.userid, expertChatRequest.sessionid, expertChatRequest.agent);
             return Ok(resposne.Content);
         }
-
+        [HttpPost("ask-quran")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> ask([FromBody] ExpertChatRequest expertChatRequest)
+        {
+            var response = await verseService.Ask(expertChatRequest.message, expertChatRequest.agent);
+            if(!response.Item2) 
+                return Ok(response.Item1);
+            return Ok("Sorry i could not find the answer of your quetion");
+           
+        }
     }
 }
--- src/infrastructure/Service/IVerseService.cs
+++ src/infrastructure/Service/IVerseService.cs
@@ -5,5 +5,6 @@ namespace infrastructure.Service
     public interface IVerseService
     {
         public Task<(string, bool)> GetVerse(string query, AgentType agentType);
+        Task<(string, bool)> Ask(string query, AgentType agentType);
     }
 }
